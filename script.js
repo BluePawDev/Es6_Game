7c8b187eef6cb7c56dbd6769c0b4a7fd9898c03b
Noah Rolf <noahrolf1@gmail.com>
2017-06-30 14:20:56 -0500

    yay

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ var app = angular.module('myApp', []);
 
 app.controller('gameController', gameController);
 
-function gameController($interval, $window){
+function gameController($interval, $timeout){
   var vm = this;
   vm.inventory = [];
   vm.balance = 100;
   vm.gameStop = true;
-  vm.counter;
+  vm.clickCounter = 0;
+  let expire = false;
+
+  function expiredFruit(){
+    if (expire === true) {
+      for (let item of vm.inventory) {
+        if (item.category == 'fruit') {
+          item.owned = 0;
+        }; //end if
+      }; //end for
+      expire = false;
+    }; //end if
+  };
 
   vm.startGame = function(){
     for (let item of vm.inventory) {
@@ -16,27 +28,36 @@ function gameController($interval, $window){
       item.setPrice();
       item.changePrice();
     };
+    setTimeout(function(){
+      vm.gameStop = true;
+      alert('Game Over!');
+    }, 900000);
   }
 
     vm.sellItem = function(index){
-      if (!gameStop) {
+      if (!vm.gameStop) {
         let item = vm.inventory[index];
         if (item.owned > 0) {
           item.owned --;
           vm.balance += item.price;
         }
+        vm.clickCounter++;
+        item.expire();
+        expiredFruit();
       }
     }
 
   vm.buyItem = function(index){
-    if (!gameStop) {
+    if (!vm.gameStop) {
       let item = vm.inventory[index];
       if (vm.balance >= item.price) {
         vm.balance -= item.price;
         item.owned ++;
         averager(index)
       }
-      item.bought = true;
+      vm.clickCounter++;
+      item.expire();
+      expiredFruit();
     }
   }
 
@@ -65,46 +86,97 @@ function gameController($interval, $window){
        $interval(() => {
          let x = randomNumber(0, 9);
          if (x >= 5) {
-           let z = randomNumber(0.5, 1);
+           let z = randomNumber(0.1, 0.9);
            this.price += z
            if (this.price >= 9.99) {
              this.price -= z;
-             this.price -= randomNumber(0.5, 1);
+             this.price -= randomNumber(0.1, 0.9);
            }
          }
          else {
-           let y = randomNumber(0.5, 1);
+           let y = randomNumber(0.1, 0.9);
            this.price -= y
            if (this.price <= 0.5) {
              this.price += y
-             this.price += randomNumber(0.5, 1);
+             this.price += randomNumber(0.1, 0.9);
            }
          }
       }, 15000);
     };
-  } //class end
+  }; //class end
+
+  class Fruit extends Commodity {
+    constructor(name, img, category){
+      super(name, img, category);
+    };
+    setPrice(){
+      super.setPrice();
+    };
+    changePrice(){
+      super.changePrice();
+    };
+    expire(){
+      if (this.category == 'fruit') {
+        if (vm.clickCounter >= 10) {
+          expire = true;
+          vm.clickCounter = 0;
+        };
+      };
+    };
+  };
+
+  class Elec extends Commodity {
+    constructor(name, img, category){
+      super(name, img, category);
+    };
+    setPrice(){
+      super.setPrice();
+    };
+    changePrice(){
+      $interval(() => {
+        let z = randomNumber(0.1, 0.3);
+        this.price += z;
+     }, 15000);
+    }
+  };
+
+  class Collect extends Commodity {
+
+  };
 
   function randomNumber(min, max){
       return mathNumber = (Math.random() * (max - min));
       }
 
-  const itemsArray = [
+  const fruitArray = [
               ['apple', 'images/apple.png', 'fruit'],
               ['orange', 'images/orange.png', 'fruit'],
               ['banana', 'images/bananas.png', 'fruit'],
-              ['grapes', 'images/grapes.png', 'fruit'],
+              ['grapes', 'images/grapes.png', 'fruit']
+            ];
+  const elecArray = [
               ['lamp', 'images/lamp.png', 'Small electronic'],
               ['clock', 'images/clock.png', 'Small electronic'],
               ['toaster', 'images/toaster.png', 'Small electronic'],
-              ['blue ray player', 'images/blu-ray-player.png', 'Small electronic'],
+              ['blue ray player', 'images/blu-ray-player.png', 'Small electronic']
+            ];
+  const collectArray = [
               ['comic book', 'images/comic-books.png', 'collectible'],
               ['fancy stuffed animal', 'images/stuffed-animal.png', 'collectible'],
               ['jewelry', 'images/jewelry.png', 'collectible'],
               ['wine', 'images/wine.png', 'collectible']
             ]
 
-  for ( let commodity of itemsArray) {
-    let item = new Commodity(...commodity);
-    vm.inventory.push(item);
-  }
+  for ( let commodity of fruitArray) {
+    let fruit = new Fruit(...commodity);
+    vm.inventory.push(fruit);
+  };
+  for ( let commodity of elecArray) {
+    let elec = new Elec(...commodity);
+    vm.inventory.push(elec);
+  };
+  for ( let commodity of collectArray) {
+    let collect = new Collect(...commodity);
+    vm.inventory.push(collect);
+  };
 }
